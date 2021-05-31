b8fbedd227894183abf6815788832917fd6050c5
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 06:20:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,4 +8,6 @@ def meal_choice(veg1, veg2, protein = 'tofu')
   return meal
 end
 
-meal_choice("broccoli", "macaroni", "roast beef")
+meal_choice('carrots', 'string beans', 'lentils')
+
+meal_choice('carrots', 'string beans')
